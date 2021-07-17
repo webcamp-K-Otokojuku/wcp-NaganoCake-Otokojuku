118b15ce0d598d5e672855557cb5b610288095f1
tomoyukikubota <tomoyukikubota0119@gmail.com>
2021-07-17 03:46:42 +0000

    [add]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,20 @@
 class Admin::OrdersController < ApplicationController
+# ログイン済管理者のみにアクセスを許可する
+  before_action :authenticate_admin!
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,49 @@
 Rails.application.routes.draw do
-  
+
   root to: 'homes#top'
   get '/homes/about' => 'homes#about'
-  
-  devise_for :admins
+
+  devise_for :admins, controller: {
+      sessions:      'admins/sessions',
+      passwords:     'admins/passwords',
+      registrations: 'admins/registrations'
+  }
+
   devise_for :customers
-  
+
   # ここから顧客側
   resources :items, only:[:index,:show]
-  
+
   resources :orders, only:[:new,:create,:index,:show]
   post '/orders/confirm' => 'orders#confirm'
   get '/orders/complete' => 'orders#complete'
-  
+
   resource :customers, only:[:show,:edit,:update]
   get '/customers/hide' => 'customers#hide'
   patch '/customers/hide' => 'customers#hide'
-  
+
   resources :shopping_addresses, except:[:show]
-  
+
   resources :cart_items, only:[:index,:update,:destroy]
   delete '/cart_items/all_destroy' => 'cart_items#all_destroy'
-  
-  
+
+
   # ここから管理者側
   namespace :admin do
-    resources :orders, only:[:index,:show,:update] do 
+    resources :orders, only:[:index,:show,:update] do
       resources :order_items, only:[:update]
-    end 
-    
+    end
+
     resources :items, except:[:destroy]
-    
+
     resources :genres, only:[:index,:create,:edit,:update]
-    
+
     resources :customers, only:[:index,:show,:edit,:update]
-    
-  end 
-  
-  
-  
-  
-  
+
+  end
+
+
+
+
+
 end
