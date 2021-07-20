e91773fe9361caf206e627cb58b1b01126080d76
sykg1204yuki <y.yuki040507@gmail.com>
2021-07-20 06:56:35 +0000

    [add]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -7,15 +7,27 @@ class Admin::CustomersController < ApplicationController
 
   def show
     @customer = Customer.find(params[:id])
-    
+
   end
 
   def edit
     @customer = Customer.find(params[:id])
-    
+
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customers_path(customer.id)
+
+  end
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :address, :postcode, :tel)
+
   end
 
 end
