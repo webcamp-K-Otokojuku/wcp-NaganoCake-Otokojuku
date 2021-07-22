01bc1d20e0bf7c5b166c9eb8d0e74e3c1e12c344
tomoyukikubota <tomoyukikubota0119@gmail.com>
2021-07-22 02:29:46 +0000

    [add]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,14 +3,21 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @orders = Order.page(params[:page]).reverse_order
+    @orders = Order.page(params[:page]).per(10)
 
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
 
+  # https://qiita.com/taro03/items/ee4cba199e54dd0fe7b1
   def update
+    @order = Order.find(params[:id])
+    status = params[:order] [:status].to_i
+    @order.update(status: status)
+    redirect_to admin_order_path(@order)
   end
 
   private
