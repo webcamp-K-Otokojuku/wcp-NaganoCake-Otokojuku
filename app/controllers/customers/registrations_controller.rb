098b07fbd6c2d0073670bdf7e2baa013520ee710
elc-p <yomtype10@gmail.com>
2021-07-26 08:33:13 +0000

    [Update]

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -51,8 +51,8 @@ class Customers::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
+  # def after_sign_in_path_for(resource)
+  #   customers_path
   # end
 
   # The path used after sign up for inactive accounts.
